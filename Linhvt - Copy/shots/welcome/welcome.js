33e7ac54074936032f04be8483282ba81562caed
chihy2022 <chihy2022@gmail.com>
2026-07-20 15:35:43 +0700

    xóa

--- Linhvt - Copy/shots/welcome/welcome.js
+++
@@ -1,9 +0,0 @@
-window.welcomeInit = function() {
-    // 1. Hiển thị tên User
-    const userData = localStorage.getItem("Unime_UserData");
-    if (userData) {
-        const user = JSON.parse(userData);
-        const nameEl = document.getElementById('user-welcome-name');
-        if (nameEl) nameEl.innerText = user.name;
-    }
-}
